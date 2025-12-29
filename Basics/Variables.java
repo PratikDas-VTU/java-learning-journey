9a6240e4154a6b7a1bf155a132be8a5c7c61fffc
Pratik Das <vtu30493@veltech.edu.in>
2025-12-29 22:49:08 +0530

    Add Variables class demonstrating basic data types and operations

---
+++ Basics/Variables.java
@@ -0,0 +1,30 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // String variable
+        String name = "Das";
+
+        // Integer variables
+        int age = 20;
+        int a = 25;
+        int b = 10;
+
+        // Double variable
+        double pi = 3.14;
+
+        // Boolean variable
+        boolean isStudent = true;
+
+        // Operations
+        int sum = a + b;
+        int product = a * b;
+
+        // Output
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Value of Pi: " + pi);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
+    }
+}
