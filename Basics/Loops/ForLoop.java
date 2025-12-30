f704260855f6000e17b681ce0ce06d56aa0f58c7
Pratik Das <vtu30493@veltech.edu.in>
2025-12-30 22:26:47 +0530

    Add basic for loop examples in Java
    Renamed class from 'forLoop' to 'ForLoop' to follow Java naming conventions.

---
+++ Basics/Loops/ForLoop.java
@@ -0,0 +1,18 @@
+package Loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        
+        // Print Hello World 5 times
+        for(int i = 1; i <= 5; i++){
+            System.out.println("Hello World");
+        }
+
+        System.out.println();
+        
+        // Print numbers from 1 to 10
+        for(int i = 1; i <= 10; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
