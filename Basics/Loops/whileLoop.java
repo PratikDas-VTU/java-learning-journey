106a43246486cd99593aaa9ac3b0cf574c6820af
Pratik Das <vtu30493@veltech.edu.in>
2026-01-03 14:46:37 +0530

    Add while and do-while loop example in Java

---
+++ Basics/Loops/whileLoop.java
@@ -0,0 +1,15 @@
+package Loops;
+public class whileLoop {
+    public static void main(String[] args){
+        int i = 0;
+        int j = 0;
+        while(i<5){
+            System.out.println("Hello World!");
+            i++;
+        }
+        do{
+            System.out.println("Do While Loop!");
+            j++;
+        }while(j<5);
+    }
+}
