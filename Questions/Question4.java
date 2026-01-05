75970743a32c16a3f18316cdcff2d6531702da3c
Pratik Das <vtu30493@veltech.edu.in>
2026-01-05 12:01:06 +0530

    Create Question4.java

---
+++ Questions/Question4.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class Question4 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 0; i <=n;i++){
+            sum = sum +i;
+        }
+        System.out.println("Sum of first "+n+" Natural numbers is:"+sum);
+    }
+}
