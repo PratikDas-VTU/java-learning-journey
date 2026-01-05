302561d490b091f9737fd540bb74390ceb9bb3c5
Pratik Das <vtu30493@veltech.edu.in>
2026-01-05 11:48:33 +0530

    Create Switch.java

---
+++ Basics/Switch.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Switch {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Select Button 1 / 2 / 3:");
+        int button = sc.nextInt();
+        switch (button){
+            case 1:
+                System.out.println("Hello!!");
+                break;
+            case 2:
+                System.out.println("Namaste!!");
+                break;
+            case 3:
+                System.out.println("Hallo!!(German)");
+                break;
+            default:
+                System.out.println("! Invalid Button !");
+        }
+    }
+}
