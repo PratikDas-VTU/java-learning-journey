757d87cfebbfd97dea5c25dcf9856dc57e18dc02
Pratik Das <vtu30493@veltech.edu.in>
2026-02-22 22:24:37 +0530

    Added BroCode folder with initial Java file

---
+++ BroCode/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[]args){
+        // Integer Datatype
+        int age = 20;
+        int year = 2026;
+        int weight = 58;
+
+        System.out.println(age);
+        System.out.println(year);
+        System.out.println(weight);
+        System.out.println();
+
+    }
+}
