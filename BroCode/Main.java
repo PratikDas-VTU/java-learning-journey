97bceff51f63a1810d34348165f14cc21bc5a681
Pratik Das <vtu30493@veltech.edu.in>
2026-02-22 23:40:50 +0530

    revised datatype in Main.java from Brocode

--- BroCode/Main.java
+++ BroCode/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[]args){
+        System.out.println("Primitive Datatype");
         // Integer Datatype
+        System.out.println("Integer Datatype");
         int age = 20;
         int year = 2026;
         int weight = 58;
@@ -8,7 +10,51 @@ public class Main {
         System.out.println(age);
         System.out.println(year);
         System.out.println(weight);
-        System.out.println();
+        
+        // Double or float datatype
+        System.out.println("Double or float datatype");
+        double pi = 3.14;
+        double price = 45.433;
 
+        System.out.println(pi);
+        System.out.println(price);
+
+        //char datatype
+        System.out.println("char datatype");
+        char grade='A';
+        char currency = 'R';
+
+        System.out.println(grade);
+        System.out.println(currency);
+
+        // Boolean datatype
+        System.out.println("Boolean datatype");
+        boolean isStudent = true;
+        boolean isOnline = false;
+
+        if (isStudent){
+            System.out.println("You are a student.");
+        } else {
+            System.out.println("You are not a student.");
+        }
+        if (isOnline){
+            System.out.println("You are Online.");
+        } else {
+            System.out.println("You are not Online.");
+        }
+
+        // Reference Datatype
+        System.out.println("-------------------------------------------");
+        System.out.println("Reference Datatype");
+        // String datatype
+        String name = "Pratik Das";
+        String food = "Dal";
+        String email = "testing@123gmail.com";
+
+
+        System.out.print(name);
+        System.out.println(food);
+        System.out.println("My email address is: "+ email);
+        System.out.println("My name is " + name + " and I am "+age+" years old.");
     }
 }
