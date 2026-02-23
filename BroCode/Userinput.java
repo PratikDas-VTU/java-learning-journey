c873557b3836c22876ac6d578337b5bcb7c70256
Pratik Das <vtu30493@veltech.edu.in>
2026-02-23 23:22:50 +0530

    Solved the user input bug.

--- BroCode/Userinput.java
+++ BroCode/Userinput.java
@@ -3,16 +3,21 @@ public class Userinput {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter You name: ");
+        System.out.print("Enter You name: ");
         String name = sc.nextLine(); // For a full next line string input
 
-        System.out.println("Enter you age: ");
+        System.out.print("Enter you age: ");
         int age = sc.nextInt(); // for an integer input
 
-        System.out.println("Enter Your Gpa: ");
+        sc.nextLine(); // Solving the user input bug 
+
+        System.out.print("Enter your favorite Color :");
+        String color = sc.nextLine();
+
+        System.out.print("Enter Your Gpa: ");
         double gpa = sc.nextDouble(); // for double / floating point number
 
-        System.out.println("Hello "+name+"."+" Your age is "+age+"."+" Your current gpa is "+gpa+".");
+        System.out.print("Hello "+name+"."+" Your age is "+age+"."+" Your current gpa is "+gpa+"."+" Your Favorite Color is "+ color + ".");
     }
 }
  
