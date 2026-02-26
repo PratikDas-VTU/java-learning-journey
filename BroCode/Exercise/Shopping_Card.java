00f08dbc9566fc7694d91d1610cf187c2b823b6d
Pratik Das <vtu30493@veltech.edu.in>
2026-02-26 22:07:19 +0530

    Completed a shopping cart problem

--- BroCode/Exercise/Shopping_Card.java
+++ BroCode/Exercise/Shopping_Card.java
@@ -1,3 +1,23 @@
+import java.util.*;
 public class Shopping_Card {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+
+        System.out.println("Welcome to Das' Stationary Store !!");
+        System.out.println("Please enter the item you wanna buy :");
+        String item = sc.nextLine();
+        System.out.println("What is the price of each "+ item);
+        double price = sc.nextDouble();
+
+        System.out.println("Enter the number of quantities: ");
+        int quantity = sc.nextInt();
+
+        double bill = price * quantity;
+
+        System.out.println("Your total bill is "+bill);
+
+        System.out.println("Thank you for shopping with Das' Stationary Store !!");
+    }
 }
